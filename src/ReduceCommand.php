2cefc2e0a33191e081ad3558ab6ec26f51ef6486
Stephan Tijink <stijink@googlemail.com>
2017-03-09 19:58:26 +0100

    Fixes

--- src/ReduceCommand.php
+++ src/ReduceCommand.php
@@ -2,6 +2,7 @@
 
 namespace ICalReducer;
 
+use ICal\EventObject;
 use ICal\ICal;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -37,19 +38,49 @@ class ReduceCommand extends Command
         $interval = $iCal->eventsFromRange(date('Y-m-d'), false);
         $events = $iCal->sortEventsWithOrder($interval);
 
-        $start = '
+        $start = "
 BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:https://github.com/stijink/ical-reducer
-METHOD:PUBLISH
-';
+METHOD:PUBLISH";
         file_put_contents($destination, trim($start));
 
         foreach ($events as $event) {
-            $eventData = $event->printData("%s: %s\n");
-            file_put_contents($destination, "BEGIN:VEVENT\n" . $eventData . "END:VEVENT\n", FILE_APPEND);
+            $eventData = $this->formatEvent($event);
+            file_put_contents($destination, "\nBEGIN:VEVENT\n" . $eventData . "END:VEVENT", FILE_APPEND);
         }
 
-        file_put_contents($destination, "END:VCALENDAR\n", FILE_APPEND);
+        file_put_contents($destination, "\nEND:VCALENDAR", FILE_APPEND);
+    }
+
+    private function formatEvent(EventObject $event) : string
+    {
+        $data = array(
+            'SUMMARY'       => $event->summary,
+            'DTSTART'       => $event->dtstart,
+            'DTEND'         => $event->dtend,
+            'DURATION'      => $event->duration,
+            'DTSTAMP'       => $event->dtstamp,
+            'UID'           => $event->uid,
+            'CREATED'       => $event->created,
+            'LAST-MODIFIED' => $event->lastmodified,
+            'DESCRIPTION'   => $event->description,
+            'LOCATION'      => $event->location,
+            'SEQUENCE'      => $event->sequence,
+            'STATUS'        => $event->status,
+            'TRANSP'        => $event->transp,
+            'ORGANIZER'     => $event->organizer,
+            'ATTENDEE'      => $event->attendee,
+        );
+
+        $data   = array_map('trim', $data); // Trim all values
+        $data   = array_filter($data);      // Remove any blank values
+        $output = '';
+
+        foreach ($data as $key => $value) {
+            $output .= "${key}:${value}\n";
+        }
+
+        return $output;
     }
 }
